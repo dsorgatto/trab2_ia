959717fcb9ff6fd4049ff19e491147e316e736da
201319030386 <201319030386@localhost.localdomain>
2015-06-09 10:46:04 -0400

    TRabalho

--- iris/trabalho.py
+++ iris/trabalho.py
@@ -13,35 +13,12 @@ from sklearn import cross_validation       # Para usar o kfold
 ##################################
 ## Funcoes
 ##################################
-def arvore_de_decisao(classes,valores,kf):
-    teste=[]
-    for treino_ind, teste_ind in kf:
-        treino_ind=list(treino_ind)
-        teste_ind=list(teste_ind)
-        clf = tree.DecisionTreeClassifier()
-        clf.fit(valores.iloc[treino_ind,:],classes.iloc[treino_ind])
-        teste.append(accuracy_score(classes.iloc[teste_ind], clf.predict(valores.iloc[teste_ind,:])))
-    return np.mean(teste)
-
-def naive_bayes(classes,valores,kf):
-    teste=[]
-    for treino_ind, teste_ind in kf:
-        treino_ind=list(treino_ind)
-        teste_ind=list(teste_ind)
-        gnb = GaussianNB()
-        gnb.fit(valores.iloc[treino_ind,:],classes.iloc[treino_ind])
-        teste.append(accuracy_score(classes.iloc[teste_ind], gnb.predict(valores.iloc[teste_ind,:])))
-    return np.mean(teste)
-
-def knn(classes,valores,kf):
-    
-
 
 ##################################
 ## Iris
 ##################################
 #Abrindo dados com numpy
-iris=pd.read_csv('iris/iris.data',header=None)
+iris=pd.read_csv('iris.data',header=None)
 
 valores = iris.iloc[:,0:4]
 classes = iris.iloc[:,4]
@@ -66,7 +43,6 @@ for i in ks:
     treino.append(accuracy_score(classes.iloc[treino_ind], knn.predict(valores.iloc[treino_ind,:])))
     knn_acu_test.append(np.mean(teste))
     knn_acu_train.append(np.mean(treino))
-  return [knn_acu_test,knn_acu_train,ks]
 
 plt.clf()
 plt.scatter(ks, knn_acu_test,c="red")
@@ -78,34 +54,33 @@ plt.savefig('knn.png')
 acuracias.append(max(knn_acu_test))
 k_usado=ks[knn_acu_test.index(max(knn_acu_test))]
 
-
 #classificador naive bayes
-acuracias.append(naive_bayes(classes,valores,kf))
+teste=[]
+for treino_ind, teste_ind in kf:
+  treino_ind=list(treino_ind)
+  teste_ind=list(teste_ind)
+  gnb = GaussianNB()
+  gnb.fit(valores.iloc[treino_ind,:],classes.iloc[treino_ind])
+  teste.append(accuracy_score(classes.iloc[teste_ind], gnb.predict(valores.iloc[teste_ind,:])))
+
+acuracias.append(naive_bayes(np.mean(teste))
 
 #classificador Decision Tree
-acuracias.append(arvore_de_decisao(classes,valores,kf))
+teste=[]
+for treino_ind, teste_ind in kf:
+  treino_ind=list(treino_ind)
+  teste_ind=list(teste_ind)
+  clf = tree.DecisionTreeClassifier()
+  clf.fit(valores.iloc[treino_ind,:],classes.iloc[treino_ind])
+  teste.append(accuracy_score(classes.iloc[teste_ind], clf.predict(valores.iloc[teste_ind,:])))
+
+acuracias.append(np.mean(teste))
 
 plt.clf()
 plt.bar([1,2,3],acuracias)
 plt.xlim(0.5, 4.5)
 plt.ylim(0.8, 1)
 plt.xticks([1.4,2.4,3.4],["Knn","Naive Bayes","Decision Tree"])
-plt.savefig('iris/acuracias.png')
+plt.savefig('acuracias.png')
 
 print "Fim"
-
-#Abrindo dados com numpy
-
-car=pd.read_csv('car_evaluation/car.data',header=None)
-
-print car.iloc[0:5,:]
-
-valores = car.iloc[:,0:6]
-classes = car.iloc[:,6]
-
-#usando k-fold
-kf = cross_validation.KFold(len(classes), n_folds=10)
-acuracias=[]
-
-#classificador knn
-knn_acu_test,knn_acu_train,ks=knn(classes,valores,kf)
