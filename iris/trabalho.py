fb8a01d9b3cb9ec4ca3764201f139f3ee5091915
Augusto Ribas <ribas.aca@gmail.com>
2015-06-10 18:12:54 -0400

    Desvios

--- iris/trabalho.py
+++ iris/trabalho.py
@@ -21,11 +21,14 @@ classes = iris.iloc[:,4]
 
 #usando k-fold
 kf = cross_validation.KFold(len(classes), n_folds=10)
-acuracias=[]
+acuracias_media=[]
+acuracias_std=[]
 
 #classificador knn
-knn_acu_train=[]
-knn_acu_test=[]
+knn_acu_train_media=[]
+knn_acu_train_std=[]
+knn_acu_test_media=[]
+knn_acu_test_std=[]
 ks=[3,5,7,9,11]
 
 for i in ks:
@@ -38,20 +41,23 @@ for i in ks:
     knn.fit(valores.iloc[treino_ind,:],classes.iloc[treino_ind])
     teste.append(accuracy_score(classes.iloc[teste_ind], knn.predict(valores.iloc[teste_ind,:])))
     treino.append(accuracy_score(classes.iloc[treino_ind], knn.predict(valores.iloc[treino_ind,:])))
-  knn_acu_test.append(np.mean(teste))
-  knn_acu_train.append(np.mean(treino))
+  knn_acu_test_media.append(np.mean(teste))
+  knn_acu_train_media.append(np.mean(treino))
+  knn_acu_test_std.append(np.std(teste))
+  knn_acu_train_std.append(np.std(treino))
 
 plt.clf()
-plt.scatter(ks, knn_acu_test,c="red")
-plt.plot(ks, knn_acu_test,c="red")
-plt.scatter(ks, knn_acu_train,c="blue")
-plt.plot(ks, knn_acu_train,c="blue")
+plt.scatter(ks, knn_acu_test_media,c="red")
+plt.plot(ks, knn_acu_test_media,c="red")
+plt.scatter(ks, knn_acu_train_media,c="blue")
+plt.plot(ks, knn_acu_train_media,c="blue")
 plt.ylabel('Acuracia')
 plt.xlabel('Numero de vizinhos mais proximos')
 plt.savefig('knn.png')
 
-acuracias.append(max(knn_acu_test))
-k_usado=ks[knn_acu_test.index(max(knn_acu_test))]
+acuracias_media.append(max(knn_acu_test_media))
+acuracias_std.append(max(knn_acu_test_std))
+k_usado=ks[knn_acu_test_media.index(max(knn_acu_test_media))]
 
 #classificador naive bayes
 teste=[]
@@ -62,7 +68,8 @@ for treino_ind, teste_ind in kf:
   gnb.fit(valores.iloc[treino_ind,:],classes.iloc[treino_ind])
   teste.append(accuracy_score(classes.iloc[teste_ind], gnb.predict(valores.iloc[teste_ind,:])))
 
-acuracias.append(np.mean(teste))
+acuracias_media.append(np.mean(teste))
+acuracias_std.append(np.std(teste))
 
 #classificador Decision Tree
 teste=[]
@@ -79,12 +86,12 @@ tree.export_graphviz(clf, out_file='arvore.dot')
 os.system("dot -Tps arvore.dot -o arvore.eps")
 os.system("rm arvore.dot")
 
-acuracias.append(np.mean(teste))
-
+acuracias_media.append(np.mean(teste))
+acuracias_std.append(np.std(teste))
 plt.clf()
-plt.bar([1,2,3],acuracias)
+plt.bar([1,2,3],acuracias_media,yerr=acuracias_std)
 plt.xlim(0.5, 4.5)
-plt.ylim(0.8, 1)
+plt.ylim(0.8, 1.04)
 plt.xticks([1.4,2.4,3.4],["Knn","Naive Bayes","Decision Tree"])
 plt.ylabel('Probabilidade')
 plt.xlabel('Algoritimos')
